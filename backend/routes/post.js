3bd9c9b9643b9bc2c9b3f4eb0ddd19f182cc47d3
AymaBA <aymaprod12@gmail.com>
2021-03-18 00:03:42 +0100

    site fonctionnel

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,64 +1,74 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fileupload = require('express-fileupload');
-const path = require('path');
-const Posts = require('../models/Posts');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fileupload = require("express-fileupload");
+const path = require("path");
+const Posts = require("../models/Posts");
 
-const io = require('socket.io')(5100);
+const io = require("socket.io")(5100);
 const router = express.Router();
 
-io.on('connect', () => console.log('Client is connected'));
+io.on("connect", () => console.log("Client is connected"));
 
 router.use(bodyParser.json());
 router.use(fileupload());
 
 router.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	res.header('Access-Control-Allow-Credentials', 'true');
-	next();
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
 
-router.post('/posts', (req, res) => {
-	const { title, content } = req.body;
-	const errors = [];
-	let succes = {success : false , msg: null};
-	if (!(title && content && req.files)) {
-		errors.push({ msg: 'Veuillez remplire tous les champs.' });
-	} else {
-		let file = req.files;
-		console.log({ img: file.img.name });
-		if (![".png", ".jpg", ".gif", ".jpeg"].includes(path.extname(file.img.name))) {
-			errors.push({ msg: "Votre image n'est pas au bon format." });
-		} else if (req.files.img.size > 2000000) {
-			errors.push({ msg: "Votre image est trop grosse , la taille maximale est 2mo." });
-		} else {
+router.post("/posts", (req, res) => {
+  console.log(req.body);
+  const { title, content } = req.body;
+  const errors = [];
+  let succes = { success: false, msg: null };
+  if (!(title && content)) {
+    errors.push({ msg: "Veuillez remplire tous les champs." });
+  } else {
+    let file = req.files;
+    if (
+      ![".png", ".jpg", ".gif", ".jpeg"].includes(path.extname(file.img.name))
+    ) {
+      errors.push({ msg: "Votre image n'est pas au bon format." });
+    } else if (req.files.img.size > 2000000) {
+      errors.push({
+        msg: "Votre image est trop grosse , la taille maximale est 2mo.",
+      });
+    } else {
+      const file = req.files.img;
+      let newPost = new Posts({ title, content });
+      const image = newPost._id + path.extname(file.name);
 
-			const file = req.files.img;
-			let newPost = new Posts({ title, content });
-			const image = newPost._id + path.extname(file.name);
-
-			file.mv(`./user-data/post-images/${image}`, err => {
-				if (err){
-					errors.push({ msg: 'L\'image n\'a pas été envoyée, veuillez réessayer.' });
-				}
-				else {
-					succes = {success : true ,msg: "Post publié" }
-					newPost = new Posts({ title, content, image: newPost._id + path.extname(file.name) });
-					newPost.save();
-					io.emit('newArticle', newPost);
-					console.log('Post send');
-				}
-			});
-		}
-	}
-	res.json({ succes, errors });
+      file.mv(`./user-data/post-images/${image}`, (err) => {
+        if (err) {
+          errors.push({
+            msg: "L'image n'a pas été envoyée, veuillez réessayer.",
+          });
+        } else {
+          succes = { success: true, msg: "Post publié" };
+          newPost = new Posts({
+            title,
+            content,
+            image: newPost._id + path.extname(file.name),
+          });
+          newPost.save();
+          io.emit("newArticle", newPost);
+          console.log("Post send");
+        }
+      });
+    }
+  }
+  res.json({ succes, errors });
 });
 
-router.get('/posts', (req, res) => Posts.find({}, (err, result) => {
-	res.json(result);
-	console.log('GET article');
-}).sort({ $natural: -1 }));
+router.get("/posts", (req, res) =>
+  Posts.find({}, (err, result) => {
+    res.json(result);
+    console.log("GET article");
+  }).sort({ $natural: -1 })
+);
 
 module.exports = router;
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,21 +8,20 @@
 // 	})
 // }
 
-
 const section1 = document.querySelector(".section1");
 const socket = io.connect("http://127.0.0.1:5100");
 const form = document.querySelector(".form-posts");
 
 const data = async () => {
-	return (await fetch("http://127.0.0.1:5000/posts")).json();
+  return (await fetch("http://127.0.0.1:5000/posts")).json();
 };
 
 (async () => {
-	let articles = await data();
-	articles = articles;
-	for (let i = 0; i < articles.length; i++) {
-		const element = articles[i];
-		section1.innerHTML += `
+  let articles = await data();
+  articles = articles;
+  for (let i = 0; i < articles.length; i++) {
+    const element = articles[i];
+    section1.innerHTML += `
 		<div class="card" >
 			<img src="http://localhost:5000/static/post-images/${element.image}" class="card-img-top" alt="...">
 			<div class="card-body">
@@ -33,13 +32,12 @@ const data = async () => {
 			</div>	
 		</div>
 		`;
-	}
-})().catch(error => console.error(error));
-
+  }
+})().catch((error) => console.error(error));
 
 socket.on("newArticle", (data) => {
-	console.log("socket : " + data);
-	$(".section1").prepend(`
+  console.log("socket : " + data);
+  $(".section1").prepend(`
 		<div class="card" >
 			<img src="http://localhost:5000/static/post-images/${data.image}" class="card-img-top" alt="...">
 			<div class="card-body">
@@ -53,33 +51,35 @@ socket.on("newArticle", (data) => {
 });
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	document.querySelector(".btn-submit").disabled = true;
-	let formData = new FormData(form);
-	// formData.append("img", document.querySelector(".custom-file-input").files[0]);
-	fetch('http://127.0.0.1:5000/posts', {
-		method: 'post',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body:formData /*JSON.stringify({
-			"title": document.querySelector("#exampleInputEmail1").value,
-			"content": document.querySelector("#exampleFormControlTextarea1").value
-		})*/
-	})
-		.then(response => response.json())
-		.then(response => {
-			if (response.errors.length > 0) {
-				document.querySelector(".error").innerHTML = `<div class="alert alert-danger" data-dismiss="alert" role="alert">
+  e.preventDefault();
+  document.querySelector(".btn-submit").disabled = true;
+  let formData = new FormData(form);
+  formData.append("img", document.querySelector(".custom-file-input").files[0]);
+  fetch("http://127.0.0.1:5000/posts", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: document.querySelector("#exampleInputEmail1").value,
+      content: document.querySelector("#exampleFormControlTextarea1").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.errors.length > 0) {
+        document.querySelector(
+          ".error"
+        ).innerHTML = `<div class="alert alert-danger" data-dismiss="alert" role="alert">
  																	${response.errors[0].msg}
 																</div>`;
-				document.querySelector(".btn-submit").disabled = false;
-			}
-		});
+        document.querySelector(".btn-submit").disabled = false;
+      }
+    });
 
-	// fetch('http://127.0.0.1:5000/posts', {
-	// 	method: 'post',
-	// 	body: formData
-	// });
+  fetch("http://127.0.0.1:5000/posts", {
+    method: "post",
+    body: formData,
+  });
 });
